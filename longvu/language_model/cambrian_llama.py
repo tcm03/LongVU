d5961998df215cd588080e80b071137d78815e36
tcm03 <21125019@student.hcmus.edu.vn>
2024-12-11 12:29:57 +0700

    Debugging learn

--- longvu/language_model/cambrian_llama.py
+++ longvu/language_model/cambrian_llama.py
@@ -520,7 +520,7 @@ class CambrianLlamaForCausalLM(LlamaForCausalLM, CambrianMetaForCausalLM):
             inputs_embeds = self.get_model().embed_tokens(inputs)
 
         # pyre-fixme[16]: `LlamaForCausalLM` has no attribute `generate`.
-        print('@tcm: CambrianLlamaForCausalLM.generate: position_ids.shape:', position_ids.shape)
+        print('@tcm: CambrianLlamaForCausalLM.generate: position_ids.type:', type(position_ids))
         print('@tcm: CambrianLlamaForCausalLM.generate: attention_mask.shape:', attention_mask.shape)
         print('@tcm: CambrianLlamaForCausalLM.generate: input_embeds.shape:', inputs_embeds.shape)
         return super().generate(
